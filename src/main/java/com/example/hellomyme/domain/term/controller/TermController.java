771d55f235a9c660e9bf8f5db8859f458d188a2e
서린 <madeleine05@soongsil.ac.kr>
2026-09-14 22:45:38 +0900

    fix:경로수정

--- src/main/java/com/example/hellomyme/domain/term/controller/TermController.java
+++ src/main/java/com/example/hellomyme/domain/term/controller/TermController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/terms")
+@RequestMapping("api/terms")
 @RequiredArgsConstructor
 public class TermController {
 
--- src/main/java/com/example/hellomyme/domain/user/controller/UserController.java
+++ src/main/java/com/example/hellomyme/domain/user/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
